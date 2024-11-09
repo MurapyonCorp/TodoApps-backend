e1b6711a666e0e7d9959e3585ef907341cd0e2c1
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-11-09 15:29:51 +0900

    相対パスに変更

--- src/controllers/todos/todos.controller.ts
+++ src/controllers/todos/todos.controller.ts
@@ -7,8 +7,8 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { CreateTodoInput } from 'src/dto/create-todo.input';
-import { UpdateTodoInput } from 'src/dto/update-todo.input';
+import { CreateTodoInput } from '../../dto/create-todo.input';
+import { UpdateTodoInput } from '../../dto/update-todo.input';
 import { TodosModel } from 'src/models/todos/todos.model';
 import { TodosService } from 'src/services/todos/todos.service';
 import { DeleteResult, InsertResult, UpdateResult } from 'typeorm';
