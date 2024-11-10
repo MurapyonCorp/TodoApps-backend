f22fa4d2a09d07afdd575eaf82f8a77b8d57eba5
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-11-10 17:07:03 +0900

    DBをpostgresに変更

--- dist/config/database.config.js
+++ dist/config/database.config.js
@@ -1,1 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DATABASE_CONFIG = void 0;
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)({ path: '.env' });
+const DB_CONNECTION = ['mysql', 'mariadb', 'postgres', 'mongodb'];
+const parseDBConnection = (dbConnection) => {
+    if (DB_CONNECTION.some((value) => value === dbConnection)) {
+        return dbConnection;
+    }
+    return 'mysql';
+};
+exports.DATABASE_CONFIG = {
+    DB_CONNECTION: parseDBConnection(process.env.DB_CONNECTION),
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: parseInt(process.env.DB_PORT),
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+};
 //# sourceMappingURL=database.config.js.map
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosModule } from './modules/todos/todos.module';
-// import { DATABASE_CONFIG } from './config/database.config';
+import { DATABASE_CONFIG } from './config/database.config';
 import { CountUpTimersModule } from './modules/countup-timers/countup-timers.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: DATABASE_CONFIG.DB_CONNECTION,
-    //   host: DATABASE_CONFIG.DB_HOST,
-    //   port: DATABASE_CONFIG.DB_PORT,
-    //   database: DATABASE_CONFIG.DB_DATABASE,
-    //   username: DATABASE_CONFIG.DB_USERNAME,
-    //   password: DATABASE_CONFIG.DB_PASSWORD,
-    //   entities: [__dirname + '/**/*.model{.ts,.js}'],
-    //   synchronize: true,
-    //   logging: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: DATABASE_CONFIG.DB_CONNECTION,
+      host: DATABASE_CONFIG.DB_HOST,
+      port: DATABASE_CONFIG.DB_PORT,
+      database: DATABASE_CONFIG.DB_DATABASE,
+      username: DATABASE_CONFIG.DB_USERNAME,
+      password: DATABASE_CONFIG.DB_PASSWORD,
+      entities: [__dirname + '/**/*.model{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
     TodosModule,
     CountUpTimersModule,
   ],
