c85acd5591f33a7292201f1bb7878cee2cf37dea
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2025-01-27 23:48:18 +0900

    synchronize: false

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { CountUpTimersModule } from './modules/countup-timers/countup-timers.mod
       username: DATABASE_CONFIG.DB_USERNAME,
       password: DATABASE_CONFIG.DB_PASSWORD,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: false,
       logging: true,
     }),
     TodosModule,
